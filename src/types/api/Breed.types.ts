4918227ef1cd8fd19f6ba3786b22613f31540fcb
Orestis Zinelis <zinelisorestis@gmail.com>
2025-02-02 18:31:18 +0200

    Add entities types

---
+++ src/types/api/Breed.types.ts
@@ -0,0 +1,12 @@
+export interface Breed {
+  id: string
+  weight: { imperial: string; metric: string }
+  temperament: string
+  origin: string
+  country_codes: string
+  country_code: string
+  life_span: string
+  wikipedia_url: string
+  name: string
+  description: string
+}
