5a6c8f28ca44e87834cc88e17ccf0d9fd49cde4d
Orestis Zinelis <zinelisorestis@gmail.com>
2025-02-02 18:37:01 +0200

    Add App.tsx, main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { CssBaseline, Snackbar, Alert } from '@mui/material'
+import Cats from '@pages/Cats'
+import Breeds from '@pages/Breeds'
+import BreedModal from '@src/components/BreedModal'
+import Favorites from '@pages/Favorites'
+import CatModal from '@src/components/CatModal'
+import AppTopBar from '@layouts/AppTopBar'
+import type { Page } from '@src/types/Page.types'
 
-function App() {
-  const [count, setCount] = useState(0)
+const pages: Page[] = [
+  { title: 'Cats', path: '/' },
+  { title: 'Breeds', path: '/breeds' },
+  { title: 'Favorites', path: '/favorites' }
+]
+
+export default function App() {
+  const [alert, setAlert] = useState<{ open: boolean; message: string; severity: 'success' | 'error' }>({
+    open: false,
+    message: '',
+    severity: 'success'
+  })
+
+  const showAlert = useMemo(
+    () => (message: string, severity: 'success' | 'error') => {
+      setAlert({ open: true, message, severity })
+    },
+    [setAlert]
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <CssBaseline>
+      <Router>
+        <div className="min-h-screen bg-pink-100">
+          <AppTopBar pages={pages} />
+          <div>
+            <Routes>
+              <Route path="/" element={<Navigate to="/cats" replace />} />
+              <Route path="/cats" element={<Cats />}>
+                <Route path=":catId" element={<CatModal showAlert={showAlert} />} />
+              </Route>
+              <Route path="/breeds" element={<Breeds />}>
+                <Route path=":breedId" element={<BreedModal showAlert={showAlert} />}>
+                  <Route path="cats/:catId" element={<CatModal showAlert={showAlert} />} />
+                </Route>
+              </Route>
+              <Route path="/favorites" element={<Favorites showAlert={showAlert} />}>
+                <Route path=":catId" element={<CatModal showAlert={showAlert} />} />
+              </Route>
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+      <Snackbar open={alert.open} autoHideDuration={3000} onClose={() => setAlert({ ...alert, open: false })}>
+        <Alert severity={alert.severity} onClose={() => setAlert({ ...alert, open: false })}>
+          {alert.message}
+        </Alert>
+      </Snackbar>
+    </CssBaseline>
   )
 }
-
-export default App
