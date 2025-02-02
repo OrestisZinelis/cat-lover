73b47cc07ac9c63fbc3f73010b1a9cd2717a2c89
Orestis Zinelis <zinelisorestis@gmail.com>
2025-02-02 18:30:50 +0200

    Set Paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@src': '/src',
+      '@components': '/src/components',
+      '@utils': '/src/utils',
+      '@hooks': '/src/hooks',
+      '@api': '/src/api',
+      '@layouts': '/src/layouts',
+      '@pages': '/src/pages'
+    }
+  }
 })
